7c5a313e463dfe167f6babb2de9b2e1228f3cc72
ali-elkholy <78052008+ali-elkholy@users.noreply.github.com>
2021-06-24 13:21:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function myFunctions() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function setCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
